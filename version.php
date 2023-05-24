5fbb6d475278631b603bf3ddf153172caea56a1a
Andy Cole <42874240+AndyMCole@users.noreply.github.com>
2023-05-24 09:24:15 +0100

    Updating to release 2023052401 for rebuild and redeployment

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2023052302;
+$plugin->version = 2023052401;
 $plugin->release = '401.0.1';
 $plugin->maturity = MATURITY_BETA;
 
