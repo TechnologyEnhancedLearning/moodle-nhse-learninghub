386382825fdd329c684779d1f36f3741cd586ff9
Andy Cole <42874240+AndyMCole@users.noreply.github.com>
2023-06-12 16:37:58 +0100

    Update config.php

--- config.php
+++ config.php
@@ -43,7 +43,7 @@ $CFG->debugdisplay = 1;
 
 $THEME->doctype = 'html5';
 $THEME->name = 'nhse';
-$THEME->version = '2023061201';
+$THEME->version = '2023061202';
 $THEME->parents = ['boost'];
 $THEME->regions = [];
 $THEME->sheets = [];
