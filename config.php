323954b6e47d9b1c49d99a18e2ab647c5f2cfcf2
Mahesh Murali P <maheshmuralip@gmail.com>
2023-07-13 09:02:54 +0100

    Release 2023071301 info changes

--- config.php
+++ config.php
@@ -43,7 +43,7 @@ $CFG->debugdisplay = 1;
 
 $THEME->doctype = 'html5';
 $THEME->name = 'nhse';
-$THEME->version = '2023071201';
+$THEME->version = '2023071301';
 $THEME->parents = ['boost'];
 $THEME->regions = [];
 $THEME->sheets = [];
