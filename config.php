7bdc7b524ba923afc560628279240a5c6981b231
Marcin Calka <6230943+marcinca@users.noreply.github.com>
2023-08-18 10:13:09 +0100

    DI-5598 Version bump

--- config.php
+++ config.php
@@ -43,7 +43,7 @@ $CFG->debugdisplay = 1;
 
 $THEME->doctype = 'html5';
 $THEME->name = 'nhse';
-$THEME->version = '2023081001';
+$THEME->version = '2023081801';
 $THEME->parents = ['boost'];
 $THEME->regions = [];
 $THEME->sheets = [];
