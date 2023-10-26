55f60f3fed59276032c47ece32f7d792a3b552bf
Marcin Calka <6230943+marcinca@users.noreply.github.com>
2023-10-26 11:51:51 +0100

    DI-5793 SCSS solution
    Changed SCSS instead of a template JS

--- config.php
+++ config.php
@@ -43,7 +43,7 @@ $CFG->debugdisplay = 1;
 
 $THEME->doctype = 'html5';
 $THEME->name = 'nhse';
-$THEME->version = '2023091801';
+$THEME->version = '2023102601';
 $THEME->parents = ['boost'];
 $THEME->regions = [];
 $THEME->sheets = [];
