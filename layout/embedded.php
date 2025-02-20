2c94a51280fdcd5527d14acd60977ddc29fdbabc
Paul Steven <p.steven@nhs.net>
2025-02-20 20:01:03 +0000

    Pathing, name and css updates

--- layout/embedded.php
+++ layout/embedded.php
@@ -36,6 +36,6 @@ $templatecontext = [
 ];
 
 // Include NHSUK Frontend js file
-$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/theme/nhse/node_modules/dist/nhsuk.min.js'));
+$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/theme/nhse/node_modules/nhse-tel-frontend/dist/nhsuk.min.js'));
 
 echo $OUTPUT->render_from_template('theme_nhse/embedded', $templatecontext);
